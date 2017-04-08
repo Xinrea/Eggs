ad85e28b49f54f0d32a6e68a379feb702108ebe2
Xinrea <s11s@vip.qq.com>
2017-04-08 23:52:10 +0800

    GSM基本框架修改

--- GameStateManager.c
+++ GameStateManager.c
@@ -4,7 +4,9 @@
    Date:		2017-4-8
    Purpose:		GSM */
 
-#include "System.h"
+#include "GameStateManager.h"
+#include "Level1.h"
+#include "Level2.h"
 
 //------------------------------------------------------------------------------
 // Private Consts:
@@ -13,7 +15,11 @@
 //------------------------------------------------------------------------------
 // Private Structures:
 //------------------------------------------------------------------------------
-
+typedef Sruct State{
+    int (* Previous)(void);
+    int (* Current)(void);
+    int (* Next)(void);
+};
 //------------------------------------------------------------------------------
 // Public Variables:
 //------------------------------------------------------------------------------
@@ -21,7 +27,12 @@
 //------------------------------------------------------------------------------
 // Private Variables:
 //------------------------------------------------------------------------------
-
+int (*Load )(void);
+int (*Ini )(void);
+int (*Update )(void);
+int (*Draw )(void);
+int (*Free )(void);
+int (*Unload )(void);
 //------------------------------------------------------------------------------
 // Private Function Declarations:
 //------------------------------------------------------------------------------
@@ -29,9 +40,33 @@
 //------------------------------------------------------------------------------
 // Public Functions:
 //------------------------------------------------------------------------------
+void GSM_Initialize(){
+    State.Current=State_Level1;
+    State.Next=State_Level2;
+    //More...
+}
 
+void GSM_Update(){
 
+}
 
+void State_Level1(){
+    Load=Load1;
+    Init=Init1;
+    Update=Update1;
+    Draw=Draw1;
+    Free=Free1;
+    Unload=Unload1;
+}
+
+void State_Level2(){
+    Load=Load2;
+    Init=Init2;
+    Update=Update2;
+    Draw=Draw2;
+    Free=Free2;
+    Unload=Unload2;
+}
 
 //------------------------------------------------------------------------------
 // Private Functions:
