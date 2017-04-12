c76b7e40cdf784de81fc5fdf0a7b953e876f423a
Xinrea <s11s@vip.qq.com>
2017-04-12 20:24:43 +0800

    4/12

--- main.c
+++ main.c
@@ -23,7 +23,8 @@ FILE *fp;
 //------------------------------------------------------------------------------
 // Public Variables:
 //------------------------------------------------------------------------------
-
+int Counter;
+int Lives=1;
 //------------------------------------------------------------------------------
 // Private Variables:
 //------------------------------------------------------------------------------
@@ -41,8 +42,34 @@ int main(){
     System_Initialize();
     GSM_Initialize();
     while(GS!=GS_Quit){
+        if(GS=GS_L1){
+            State.Current=State_Level1;
+            State.Next=State_Level2;
+        }
+        if(GS=GS_L2){
+            State.Previous=State.Current;
+            State.Current=State.Next;
+            State.Next=State_Quit;
+        }
 
-    }
+        GSM_Update();
+        Load();
+        do {
+            Init();
+            while (Counter > 0) {
+                Input_Handle();
+                Update();
+                Draw();
+            }
+            Lives--;
+            if(Lives>0)GS=GS_Restart;
+            else if(GS=GS_L1)GS=GS_L2;
+            else GS=GS_Quit;
+            Free();
+        }while(GS=GS_Restart);
+            Unload();
+        }
+    System_Exit();
     return 0;
 }
 
