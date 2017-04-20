6c85e97940f2352042680e3c3a343615af3690ca
Kobe1986 <18267681088@126.com>
2017-04-20 15:26:00 +0800

    111

--- Level1.c
+++ Level1.c
@@ -6,6 +6,8 @@
 
 #include "Level1.h"
 #include "stdio.h"
+#include "GameStateList.h"
+#include "System.h"
 extern FILE* fp;
 //------------------------------------------------------------------------------
 // Private Consts:
@@ -17,13 +19,13 @@ extern FILE* fp;
 
 //------------------------------------------------------------------------------
 // Public Variables:
-extern int Counter;
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
 // Private Variables:
-
 //------------------------------------------------------------------------------
+static AEGfxVertexList*	pMesh1;				// 对象1的网格(mesh)模型
+static float obj1X, obj1Y;
 
 //------------------------------------------------------------------------------
 // Private Function Declarations:
@@ -33,28 +35,48 @@ extern int Counter;
 // Public Functions:
 //------------------------------------------------------------------------------
 void Load1(void){
-	FILE *fpRead = fopen("Level1_Counter.txt", "r");
-	if (fpRead == NULL)
-	{
-		return;
-	}
-	fscanf(fpRead, "%d ", &Counter);
+    // 开始添加对象1
+	AEGfxMeshStart();
+	AEGfxTriAdd(
+		-25.5f, -25.5f, 0xFFFF0000, 0.0f, 0.0f,
+		25.5f,  0.0f, 0x00FF0000, 0.0f, 0.0f,
+		-25.5f,  25.5f, 0xFFFFFF00, 0.0f, 0.0f);
+	pMesh1 = AEGfxMeshEnd();
+	AE_ASSERT_MESG(pMesh1, "Failed to create mesh 1!!");
+
     fprintf(fp, "Level1:Load\n");
 }
 void Ini1(void){
+    // 对象1的初始位置
+	obj1X = 0.0f;
+	obj1Y = 0.0f;
+
+	// 为开始画对象做准备
+	AEGfxSetBackgroundColor(0.0f, 0.0f, 0.0f);   // 背景色RGB
+	AEGfxSetBlendMode(AE_GFX_BM_BLEND);
+
     fprintf(fp, "Level1:Ini\n");
 }
 void Update1(void){
-    	Counter--;
     fprintf(fp, "Level1:Update\n");
 }
 void Draw1(void){
+    // 画对象1
+	AEGfxSetRenderMode(AE_GFX_RM_COLOR);
+	AEGfxSetPosition(obj1X, obj1Y);
+	AEGfxTextureSet(NULL, 0, 0);
+	AEGfxSetTransparency(1);
+	AEGfxSetBlendColor(0.0f, 0.0f, 0.0, 0.0f);
+	AEGfxMeshDraw(pMesh1, AE_GFX_MDM_TRIANGLES);
+
     fprintf(fp, "Level1:Draw\n");
 }
 void Free1(void){
     fprintf(fp, "Level1:Free\n");
 }
 void Unload1(void){
+    AEGfxMeshFree(pMesh1);		// 注销创建的对象
+
     fprintf(fp, "Level1:Unload\n");
 }
 
